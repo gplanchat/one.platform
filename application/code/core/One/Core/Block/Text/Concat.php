28a948898c3bb0bdfb7ae494aa7ce85e7f591081
Gregory PLANCHAT <g.planchat@gmail.com>
2010-10-14 11:56:31 +0200

    Fixe block core/text.concat
    Signed-off-by: Gregory PLANCHAT <g.planchat@gmail.com>

--- application/code/core/One/Core/Block/Text/Concat.php
+++ application/code/core/One/Core/Block/Text/Concat.php
@@ -3,4 +3,14 @@
 class One_Core_Block_Text_Concat
     extends One_Core_Block_Text
 {
+    public function _render()
+    {
+        $render = '';
+
+        foreach ($this->getAllChildNodes() as $child) {
+            $render .= $child->render(null);
+        }
+
+        return $render;
+    }
 }
