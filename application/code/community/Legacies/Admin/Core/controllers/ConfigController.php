5cbdc1434d4418b1273bab0052902b6190f76b40
Gregory PLANCHAT <g.planchat@gmail.com>
2011-01-25 18:42:59 +0100

    Updated Copyright headers
    Signed-off-by: Gregory PLANCHAT <g.planchat@gmail.com>

--- application/code/community/Legacies/Admin/Core/controllers/ConfigController.php
+++ application/code/community/Legacies/Admin/Core/controllers/ConfigController.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This file is part of One.Platform
+ * This file is part of XNova:Legacies
  *
  * @license Modified BSD
  * @see https://github.com/gplanchat/one.platform
@@ -18,7 +18,7 @@
  *       this list of conditions and the following disclaimer in the documentation
  *       and/or other materials provided with the distribution.
  *
- *     - Neither the name of Zend Technologies USA, Inc. nor the names of its
+ *     - Neither the name of Grégory PLANCHAT nor the names of its
  *       contributors may be used to endorse or promote products derived from this
  *       software without specific prior written permission.
  *
